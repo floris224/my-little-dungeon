a02b51d9c0825474a82e176a7cf5ce985e9c0b6c
Florisvdk <floris_2@hotmail.nl>
2023-04-02 16:58:20 +0200

    box colliders assets dungeon aangezet

--- mld/Assets/Scripts/Floris/Player.cs
+++ mld/Assets/Scripts/Floris/Player.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   public int health = 100;
+    public int health;
+   //playerPref is dat de data wordt opgeslagen op de computer inplaats van op de cloud daarom is dit niet veilig
    
     public void SavePlayer(int i)
     {
         PlayerPrefs.SetFloat(i.ToString() + "PlayerX", transform.position.x);
         PlayerPrefs.SetFloat(i.ToString() + "PlayerY", transform.position.y);
         PlayerPrefs.SetFloat(i.ToString() + "PlayerZ", transform.position.z);
+
+        PlayerPrefs.SetInt(i.ToString() + "PlayerHealth", health);
 
         Debug.Log("Saved");
     }
@@ -23,8 +26,10 @@ public class Player : MonoBehaviour
             PlayerPrefs.GetFloat(i.ToString() + "PlayerY"),
             PlayerPrefs.GetFloat(i.ToString() + "PlayerZ")
 
+        
          );
         transform.position = position;
+        health = PlayerPrefs.GetInt(i.ToString() + "PlayerHealth");
         Debug.Log("loaded");
     }
 }
