ac800590c61bbaac9e4a309b7950c0399ea2b05f
Florisvdk <floris_2@hotmail.nl>
2023-04-07 15:15:38 +0200

    big duck/interact fix

--- mld/Assets/Scripts/Roel/Pickup.cs
+++ mld/Assets/Scripts/Roel/Pickup.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Pickup : MonoBehaviour
@@ -8,40 +9,78 @@ public class Pickup : MonoBehaviour
     public RaycastHit hit;
     public Camera fpsCam;
     public GameObject floor;
+    
+    public TextMeshProUGUI currentPointsText;
+    public TextMeshProUGUI targetPointsText;
+    public int targetQuestPoints = 5;
+    public int currentQuestPoints = 0;
+    public int points = 1;
 
-    // Update is called once per frame
     void Update()
     {
-        Gun();
-        Interact();
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 50f))
+            {
+                Gun();
+                Interact();
+                QuestDuck();
 
+            }
+
+
+        }
     }
 
     public void Gun()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        
+        if (hit.transform.tag == ("Weapon"))
         {
-            if (Physics.Raycast(fpsCam.transform.position, transform.forward, out hit, 50f))
+           hit.transform.SetParent(hand);
+           hit.collider.enabled = false;
+           hit.transform.position = hand.position;
+           hit.transform.rotation = hand.rotation;
+           hit.transform.GetComponent<Skiekten>().enabled = true;
 
-                if (hit.transform.tag == ("Weapon"))
-                {
-                    hit.transform.SetParent(hand);
-                    hit.collider.enabled = false;
-                    hit.transform.position = hand.position;
-                    hit.transform.rotation = hand.rotation;
-                    hit.transform.GetComponent<Skiekten>().enabled = true;
-
-                }
-
-            
         }
+
+
+        
     }
     public void Interact()
     {
-        if (hit.transform.tag == ("Interactable"))
-        {
-            floor.GetComponent<BoxCollider>().enabled = false;
-        }
+         if (hit.transform.tag == ("Interactable"))
+         {
+            floor.SetActive(false);
             
+         }
+
+        
+       
+
+    }
+    public void QuestDuck()
+    {
+        if (hit.collider.CompareTag("Quest"))
+        {
+            Destroy(hit.collider.gameObject);
+            UpdatePanelCounter();
+        }
+        
+    }
+    
+    public void UpdatePanelCounter()
+    {
+        currentQuestPoints += points;
+        currentPointsText.text = currentQuestPoints.ToString();
+        if (currentQuestPoints > targetQuestPoints)
+        {
+            targetPointsText.text = "(Completed)";
+        }
+        else
+        {
+            targetPointsText.text = "(" + currentQuestPoints + "/" + targetQuestPoints + ")";
+        }
     }
 }
